1e7a30160452ae0e154f248cee39ff0b9199de0c
colinpereira <64594861+colinpereira@users.noreply.github.com>
2021-07-01 10:57:46 -0700

    Fixed API bug for coin page

--- src/Routes/CoinPage.js
+++ src/Routes/CoinPage.js
@@ -7,7 +7,8 @@ import BackgroundImg from "../Images/cryptobckg.jpeg";
 
 function CoinPage() {
   let { id } = useParams();
-  const [coin, setCoin] = useState({});
+  const [coin, setCoin] = useState(null);
+  // coin start has to be null first to give time to fetch API data
 
   useEffect(() => {
     console.log(id);
@@ -19,66 +20,71 @@ function CoinPage() {
     );
   }, []);
 
-  return (
-    <div
-      className="coinPage-Container"
-      style={{
-        backgroundImage: `url(${BackgroundImg})`,
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-      }}
-    >
-      <div className="coinPage-Info">
-        <h1>{coin.name}</h1>
-        <img src={coin.image.large} alt="Icon" className="coinPage-Icon" />
-        <div className="coinPage-Data">
-          <div className="coinPage-Row">
-            <h3 className="coinPage-RowHeader">Symbol:</h3>
-            <h3 className="coinPage-RowData">{coin.symbol}</h3>
+  // if statement to check if data has reached, only then render the component
+  if (coin) {
+    return (
+      <div
+        className="coinPage-Container"
+        style={{
+          backgroundImage: `url(${BackgroundImg})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="coinPage-Info">
+          <h1>{coin.name}</h1>
+          <img src={coin.image.large} alt="Icon" className="coinPage-Icon" />
+          <div className="coinPage-Data">
+            <div className="coinPage-Row">
+              <h3 className="coinPage-RowHeader">Symbol:</h3>
+              <h3 className="coinPage-RowData">{coin.symbol}</h3>
+            </div>
+            <div className="coinPage-Row">
+              <h3 className="coinPage-RowHeader">Current Price:</h3>
+              <h3 className="coinPage-RowData">
+                $ {coin.market_data.current_price.usd.toLocaleString()}
+              </h3>
+            </div>
+            <div className="coinPage-Row">
+              <h3 className="coinPage-RowHeader">Market Cap:</h3>
+              <h3 className="coinPage-RowData">
+                $ {coin.market_data.market_cap.usd.toLocaleString()}
+              </h3>
+            </div>
+            <div className="coinPage-Row">
+              <h3 className="coinPage-RowHeader">Total Volume:</h3>
+              <h3 className="coinPage-RowData">
+                $ {coin.market_data.total_volume.usd.toLocaleString()}
+              </h3>
+            </div>
+            <div className="coinPage-Row">
+              <h3 className="coinPage-RowHeader">24hr High:</h3>
+              <h3 className="coinPage-RowData green">
+                $ {coin.market_data.high_24h.usd.toLocaleString()}
+              </h3>
+            </div>
+            <div className="coinPage-Row">
+              <h3 className="coinPage-RowHeader">24hr Low:</h3>
+              <h3 className="coinPage-RowData red">
+                $ {coin.market_data.low_24h.usd.toLocaleString()}
+              </h3>
+            </div>
           </div>
-          <div className="coinPage-Row">
-            <h3 className="coinPage-RowHeader">Current Price:</h3>
-            <h3 className="coinPage-RowData">
-              $ {coin.market_data.current_price.usd.toLocaleString()}
-            </h3>
-          </div>
-          <div className="coinPage-Row">
-            <h3 className="coinPage-RowHeader">Market Cap:</h3>
-            <h3 className="coinPage-RowData">
-              $ {coin.market_data.market_cap.usd.toLocaleString()}
-            </h3>
-          </div>
-          <div className="coinPage-Row">
-            <h3 className="coinPage-RowHeader">Total Volume:</h3>
-            <h3 className="coinPage-RowData">
-              $ {coin.market_data.total_volume.usd.toLocaleString()}
-            </h3>
-          </div>
-          <div className="coinPage-Row">
-            <h3 className="coinPage-RowHeader">24hr High:</h3>
-            <h3 className="coinPage-RowData green">
-              $ {coin.market_data.high_24h.usd.toLocaleString()}
-            </h3>
-          </div>
-          <div className="coinPage-Row">
-            <h3 className="coinPage-RowHeader">24hr Low:</h3>
-            <h3 className="coinPage-RowData red">
-              $ {coin.market_data.low_24h.usd.toLocaleString()}
-            </h3>
-          </div>
-        </div>
-        <Link to="/">
-          <div className="coinPage-RouteButton">Go back</div>
-        </Link>
+          <Link to="/">
+            <div className="coinPage-RouteButton">Go back</div>
+          </Link>
 
-        {/* coinName={coins.name}
+          {/* coinName={coins.name}
               coinSymbol={coins.symbol}
               price={coins.current_price}
               marketCap={coins.market_cap}
               priceChange={ */}
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return null; // if API data not fetched, return null
+  }
 }
 
 export default CoinPage;
